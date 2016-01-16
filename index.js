9c7a287e6931712a37635d5aadb17e2fdb7efbb0
Sergey Chikuyonok <serge.che@gmail.com>
2016-01-16 16:32:34 +0300

    Removed unused code

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 import View from './lib/view';
 import reel from './lib/reel';
 import wall from './lib/wall';
-import devices from './lib/sample-devices';
 
-export {View, reel, wall, devices};
+export {View, reel, wall};
