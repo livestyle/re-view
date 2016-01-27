f7de4e5eb4b759cd4f6e1043059140fbde542c6c
Sergey Chikuyonok <serge.che@gmail.com>
2016-01-28 01:48:09 +0300

    Integrated views into UI

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 'use strict';
 
-import app from './lib/app';
-import reel from './lib/view/breakpoints';
-import wall from './lib/view/device-wall';
+import tr from 'tiny-react';
+import headerUI from './lib/components/header';
+import viewUI from './lib/view';
+import {store, subscribe, dispatch, getStateValue} from './lib/app';
+import {createElement, removeElement} from './lib/utils';
 
-export {app, reel, wall};
+/**
+ * Creates complete Re:view UI and returns its parent element
+ * @return {Element}
+ */
+export default function(container) {
+    var root = createElement('div', 'emmet-re-view');
+    var header = tr.render(headerUI, store.getState());
+    var content = createElement('div', 'emmet-re-view__content');
+    var view = viewUI(content);
+    root.appendChild(header.target);
+    root.appendChild(content);
+
+    var update = function(state=store.getState()) {
+        header.update(state);
+        view.update(state);
+    };
+
+    if (container) {
+        container.appendChild(root);
+        update();
+    }
+
+    var unsubscribe = subscribe(update);
+    return {
+        element: root,
+        update,
+        destroy() {
+            removeElement(root);
+            unsubscribe();
+        }
+    };
+}
+
+export {subscribe, dispatch, getStateValue};
